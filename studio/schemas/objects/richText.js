c9890984013ac22965de4a944db2120b5c5886b6
Aarati Akkapeddi <aarati.akkapeddi@gmail.com>
2022-04-08 14:35:39 -0400

    adds lists to richText

--- studio/schemas/objects/richText.js
+++ studio/schemas/objects/richText.js
@@ -7,7 +7,10 @@ export default {
       title: 'Block',
       type: 'block',
       styles: [{title: 'Normal', value: 'normal'}],
-      lists: [],
+      lists: [
+        { title: 'Bullet', value: 'bullet' },
+        { title: 'Number', value: 'number' },
+      ],
       marks: {
         decorators: [
           {title: 'Strong', value: 'strong'},

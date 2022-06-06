11f297e2d9a16b2345b030e124b47125fa7402b9
Aarati Akkapeddi <aarati.akkapeddi@gmail.com>
2022-06-06 17:44:25 -0400

    setup page preview

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -10,33 +10,24 @@ const baseURL =
     // First, we select a specific type of document
 
     let temp;
-    if (document._type === 'page' || document._type === 'event' || document._type === 'project' || document._type === 'newsItem' || document._type === 'course' || document._type === 'partner' || document._type === 'learningResource' || document._type === 'researchThread' || document._type === 'workingGroup') {
+     
       switch (document._type) {
-        case 'page': temp = `${baseURL}/${document.slug.current}`;
-        case 'event': temp = `${baseURL}/event/${document.slug.current}`;
-        case 'project': temp = `${baseURL}/project/${document.slug.current}`;
-        case 'newsItem': temp = `${baseURL}/news/${document.slug.current}`;
-        case 'course': temp = `${baseURL}/course/${document.slug.current}`;
-        case 'partner': temp = `${baseURL}/partner/${document.slug.current}`;
-        case 'learningResource': temp = `${baseURL}/learning-resource/${document.slug.current}`;
-        case 'researchThread': temp = `${baseURL}/research-thread/${document.slug.current}`;
-        case 'workingGroup': temp = `${baseURL}/working-group/${document.slug.current}`;
-        default: temp = "";
+        case 'page': return `${baseURL}/${document.slug.current}?preview=true`;
+        case 'event':return `${baseURL}/event/${document.slug.current}?preview=true`;
+        case 'project': return `${baseURL}/project/${document.slug.current}?preview=true`;
+        case 'newsItem': return `${baseURL}/news/${document.slug.current}?preview=true`;
+        case 'course': return `${baseURL}/course/${document.slug.current}?preview=true`;
+        case 'partner': return `${baseURL}/partner/${document.slug.current}?preview=true`;
+        case 'learningResource': temp = `${baseURL}/learning-resource/${document.slug.current}?preview=true`;
+        case 'researchThread': return `${baseURL}/research-thread/${document.slug.current}?preview=true`;
+        case 'workingGroup': return `${baseURL}/working-group/${document.slug.current}?preview=true`;
+        default: temp = 'document._type';
       }
 
-      // Then we get its ID
-      let id = document._id;
-      // if it's a draft, we split its _id with the "drafts." substring, which will return an array,
-      // and get the second item in it, which will be the isolated _id without "drafts."
-      if (isDraft(id)) {
-        id = document._id.split('drafts.')[1];
-      }
+      
       // And return a template string reflecting the URL structure we want. In this case, we're doing a
       // simple conditional to return '&isDraft=true' as a param for drafts as we'll query them
       // differently in the front-end
-      return `${temp}${
-        isDraft(document._id) ? '&isDraft=true' : ''
-      }`;
-    }
-    return undefined;
+      return temp;
+    
   }
--- web/src/templates/page.js
+++ web/src/templates/page.js
@@ -3,6 +3,8 @@ import { graphql } from "gatsby";
 import Container from "../components/Container/container";
 import GraphQLErrorList from "../components/graphql-error-list";
 import SEO from "../components/seo";
+import { useLocation } from '@reach/router';
+import queryString from 'query-string';
 import Layout from "../containers/layout";
 import TranslatedTitle from "../components/TranslationHelpers/translatedTitle";
 import BlockContent from "../components/TranslationHelpers/block-content";
@@ -11,7 +13,7 @@ const client = sanityClient({
   projectId: '46orb7yp',
   dataset: 'production',
   apiVersion: '2022-03-25', // use current UTC date - see "specifying API version"!
-  token: '', // or leave blank for unauthenticated usage
+  token: 'skyfnkmqWJbwvihHkx2GQByHOktPsJB6ztzSRAfi7mZWaQegg23IaNrgFXjSxrBvL5Tli1zygeDqnUMr8QSXOZLNyjjhab5HTPsgD6QnBBxcNBOUwzGyiI69x7lpMKYhxZ94dpxLwIuVRBB1Hn47wR4rPtCpf17JGCYehmiLgCpMZrX1rzZW', // or leave blank for unauthenticated usage
   useCdn: true, // `false` if you want to ensure fresh data
 })
 export const query = graphql`
@@ -81,11 +83,18 @@ export const query = graphql`
 const PageTemplate = props => {
   const { data, errors } = props;
   const page = data && data.samplePage;
-  let previewQuery = '*[_id == "'+ page._id +'"]'
-  let previewParams = {isDraft: true};
-  client.fetch(previewQuery).then((data) => {
-    console.log(data)
-  })
+  let previewQuery = '*[_id == "drafts.'+ page._id +'"]'
+  const location = useLocation();
+  let preview = false;
+  let previewData;
+  if(location?.search){
+    preview = queryString.parse(location.search).preview;
+  }
+  if(preview){
+    client.fetch(previewQuery).then((data) => {
+      previewData = data;
+    })
+  }
   const site = (data || {}).site;
   const globalLanguages = site.languages;
   const languagePhrases = (data || {}).languagePhrases?.edges;
@@ -94,8 +103,8 @@ const PageTemplate = props => {
       <SEO title={site.title} description={site.description} keywords={site.keywords} />
       <Container>
         <h1 hidden>Welcome to {site.title}</h1>
-        <h1><TranslatedTitle translations={page.titles}/></h1>
-        <div className="top-text two-column"><BlockContent languagePhrases={languagePhrases} globalLanguages={globalLanguages} blocks={page.descriptions}/></div>
+        <h1><TranslatedTitle translations={(preview && previewData) ? previewData.titles : page.titles}/></h1>
+        <div className="top-text two-column"><BlockContent languagePhrases={languagePhrases} globalLanguages={globalLanguages} blocks={(preview && previewData) ? previewData.descriptions : page.descriptions}/></div>
       </Container>
     </Layout>
   );

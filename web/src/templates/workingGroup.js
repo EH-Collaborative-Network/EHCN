29ecba9002969eb7b92ed78869a8eb0f33dd73d8
Aarati Akkapeddi <aarati.akkapeddi@gmail.com>
2022-06-28 09:04:20 +0530

    fixes error in working group template

--- web/src/templates/workingGroup.js
+++ web/src/templates/workingGroup.js
@@ -294,7 +294,7 @@ const WorkingGroupTemplate = props => {
       <Container>
         <h1 hidden>Welcome to {site.title}</h1>
         <h1><TranslatedTitle translations={(preview && previewData) ? previewData.titles : workingGroup.titles}/></h1>
-        <div className={'subtitle'}><TranslatedPhrase translations={languagePhrases} phrase={event.studentLed ? 'studentLed' : 'facultyLed'}/></div>
+        <div className={'subtitle'}><TranslatedPhrase translations={languagePhrases} phrase={workingGroup.studentLed ? 'studentLed' : 'facultyLed'}/></div>
         <div className="top-text two-column"><BlockContent blocks={(preview && previewData) ? previewData.descriptions : workingGroup.descriptions}/></div>
         {media.length > 1 &&
           <Carousel media={(preview && previewData) ? previewData.media : workingGroup.media}/>

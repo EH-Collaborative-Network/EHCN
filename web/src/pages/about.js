9ec00a24f47dd5d35467d2780102616ab6cf26c5
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2023-09-25 18:01:37 -0400

    adds fellows to about page

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -269,7 +269,16 @@ const AboutPage = props => {
             })}
             </ul>
           </div>
-         
+          <h4>Fellows</h4>
+          <div className="staff-no-column">
+            <ul className={styles.steering}>
+            {people.map(function(node, index){
+              if(node.node.fellow){
+                return <li key={index}><Person person={node}></Person></li>;
+              }
+            })}
+            </ul>
+          </div>
         </Container>
       </Layout>
       

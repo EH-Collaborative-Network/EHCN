db9ab09a8211f5f3e7495465f5ecbe6abdc02530
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2023-10-25 09:32:58 -0400

    finessing about page

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -218,6 +218,8 @@ const AboutPage = props => {
         <Container>
           <h1 hidden>Welcome to {site.title}</h1>
           <h1><TranslatedTitle translations={(preview && previewData) ? previewData.titles : titles}/></h1>
+          <h1 className={aboutStyles.keyWords}><span onMouseOver={() => setToggle("technology")} className={`${toggle == "technology" ? aboutStyles.on : " "} ${aboutStyles.toggle}`}><TranslatedPhrase translations={languagePhrases} phrase={"technology"}/></span>, <span onMouseOver={() => setToggle("justice")} className={`${toggle == "justice" ? aboutStyles.on : " "} ${aboutStyles.toggle}`}><TranslatedPhrase translations={languagePhrases} phrase={"justice"}/></span> & <span onMouseOver={() => setToggle("creative practice")} className={`${toggle == "creative practice" ? aboutStyles.on : " "} ${aboutStyles.toggle}`}><TranslatedPhrase translations={languagePhrases} phrase={"creativepractice"}/></span></h1>
+          
           {toggle == "justice" &&
           <div className={aboutStyles.bubble +" top-text one-column"}>
             <BlockContent languagePhrases={languagePhrases} blocks={site.aboutJustice} globalLanguages={globalLanguages}/>
@@ -233,8 +235,7 @@ const AboutPage = props => {
               <BlockContent languagePhrases={languagePhrases} blocks={site.aboutTechnology} globalLanguages={globalLanguages}/>
             </div>
           }
-           <h1><span onMouseOver={() => setToggle("technology")} className={`${toggle == "technology" ? aboutStyles.on : " "} ${aboutStyles.toggle}`}><TranslatedPhrase translations={languagePhrases} phrase={"technology"}/></span>, <span onMouseOver={() => setToggle("justice")} className={`${toggle == "justice" ? aboutStyles.on : " "} ${aboutStyles.toggle}`}><TranslatedPhrase translations={languagePhrases} phrase={"justice"}/></span> & <span onMouseOver={() => setToggle("creative practice")} className={`${toggle == "creative practice" ? aboutStyles.on : " "} ${aboutStyles.toggle}`}><TranslatedPhrase translations={languagePhrases} phrase={"creativepractice"}/></span></h1>
-          
+           
           <div className="top-text about-page one-column"><BlockContent languagePhrases={languagePhrases} blocks={(preview && previewData) ? previewData.bodies : ap} globalLanguages={globalLanguages}/></div>
  
           

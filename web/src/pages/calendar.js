ba79036a3c7d74d2fe61c0d73f4f3fd56069ffe2
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2023-11-01 14:10:37 -0400

    current/upcoming for week view

--- web/src/pages/calendar.js
+++ web/src/pages/calendar.js
@@ -1,10 +1,13 @@
 import React, {useState} from "react";
 import { graphql } from "gatsby";
+import { Figure } from "../components/Figure/figure";
 import {
   mapEdgesToNodes,
   filterOutDocsWithoutSlugs,
   filterOutDocsPublishedInTheFuture
 } from "../lib/helpers";
+import { Link } from "@reach/router";
+import TranslatedTitle from "../components/TranslationHelpers/translatedTitle";
 import Container from "../components/Container/container";
 import BlockContent from "../components/TranslationHelpers/block-content";
 import GraphQLErrorList from "../components/graphql-error-list";
@@ -87,6 +90,20 @@ export const query = graphql`
           slug{
             current
           }
+          mainImage {
+            crop {
+              _key
+              _type
+              top
+              bottom
+              left
+              right
+            }
+            asset {
+              _id
+            }
+            altText
+          }
           titles{
             text
             language{
@@ -115,6 +132,7 @@ export const query = graphql`
             date
             time
           }
+          
         }
       }
     }
@@ -136,10 +154,50 @@ const CalendarPage = props => {
   const globalLanguages = site.languages;
   const languagePhrases = (data || {}).languagePhrases?.edges;
   const events = (data || {}).events?.edges;
+  events.sort(function (a, b) {
+    if (new Date(a.node.startDate.date) < new Date(b.node.startDate.date)) {
+      return 1;
+    }
+    if (new Date(a.node.startDate.date) > new Date(b.node.startDate.date)) {
+      return -1;
+    }
+    return 0;
+  });
+
+
   if (!site) {
     throw new Error(
       'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
     );
+  }
+  function isCurrentOrUpcoming(cdate, cmonth, cyear) {
+    let d = new Date();
+    let date = d.getDate();
+    let year = d.getFullYear();
+    let month = d.getMonth();
+    let is = true;
+    // cmonth = cmonth - 1;
+
+
+    if(cyear < year){
+      is = false
+    }else if(cyear > year){
+      is = true
+    }else{
+      
+      if(cmonth > month){
+        is = true
+      }else if(cmonth < month){
+          is = false
+      }else{
+          if(cdate >= date){
+            is = true
+          }else if(cdate < date){
+            is = false;
+          }
+      }
+    }
+    return is;
   }
   function getSunday() {
     // Copy date so don't modify original
@@ -277,29 +335,60 @@ const CalendarPage = props => {
             }
             {!monthView &&
             <>
-            <div className={styles.buttonWrapper}>
-            <button onClick={decrementWeek} aria-labelled-by="previous-week">
-              <span id='previous-week' hidden>previous week</span>
-              <svg width="63" height="16" viewBox="0 0 63 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M0.292892 7.29289C-0.0976295 7.68342 -0.0976295 8.31658 0.292892 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41422 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07107 0.928932C7.68054 0.538408 7.04738 0.538408 6.65685 0.928932L0.292892 7.29289ZM63 7L1 7V9L63 9V7Z" fill="#333333"/>
-              </svg>
-            </button>
-            <button onClick={incrementWeek} aria-labelled-by="next-week">
-              <span id='next-week' hidden>next week</span>
-              <svg width="61" height="16" viewBox="0 0 61 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M60.7071 8.70711C61.0976 8.31658 61.0976 7.68342 60.7071 7.29289L54.3432 0.928932C53.9526 0.538408 53.3195 0.538408 52.9289 0.928932C52.5384 1.31946 52.5384 1.95262 52.9289 2.34315L58.5858 8L52.9289 13.6569C52.5384 14.0474 52.5384 14.6805 52.9289 15.0711C53.3195 15.4616 53.9526 15.4616 54.3432 15.0711L60.7071 8.70711ZM0 9L60 9V7L0 7L0 9Z" fill="#333333"/>
-              </svg>
-            </button>
-            </div>
+            <h1>Current/Upcoming</h1>
+            <div className={styles.cardWrapper}>
+            {
+              events.toReversed().map((event, i) => {
+                let d = event.node.endDate.date.split("-")
+                if(isCurrentOrUpcoming(parseInt(d[2]),parseInt(d[1]), parseInt(d[0]))){
+                  return(
+                    <Link to={"/events/"+ event.node.slug.current}>
+                      {event.node.mainImage &&
+                        <Figure normal={true} node={event.node.mainImage} /> 
+                      }
+                      <div>
+                      {(event.node.timeZone && event.node.startDate) &&
+                        <DisplayTime event={event.node} offset={offset} languagePhrases={languagePhrases}/>
+                        }
+                        <h4><TranslatedTitle translations={ event.node.titles}/></h4>
+                        <Link className="blue-button" to={"/events/" + event.node.slug.current}>More Info</Link>
+                      </div>
                       
-            <LangContext.Consumer>
-            {theme => {
-                  console.log(sunday)
-                    return(
-                      <CreateWeek globalLanguages={globalLanguages} events={events} theme={theme} translations={languagePhrases} year={year} offset={offset} week={sunday} />
-                    )}}
-            </LangContext.Consumer>
-            </>}
+                    </Link>
+                  )
+                }
+              })
+            }
+                      
+            
+            </div>
+            <h1>Past</h1>
+            <div className={styles.cardWrapper}>
+            {
+              events.map((event, i) => {
+                let d = event.node.endDate.date.split("-")
+                if(!isCurrentOrUpcoming(parseInt(d[2]),parseInt(d[1]), parseInt(d[0]))){
+                  return(
+                    <Link to={"/events/"+ event.node.slug}>
+                      {event.node.mainImage &&
+                        <Figure normal={true} node={event.node.mainImage} /> 
+                      }
+                      <div>
+                      {(event.node.timeZone && event.node.startDate) &&
+                        <DisplayTime event={event.node} offset={offset} languagePhrases={languagePhrases}/>
+                        }
+                        <h4><TranslatedTitle translations={ event.node.titles}/></h4>
+                      <Link className="blue-button" to={"/events/" + event.node.slug.current}>More Info</Link>
+                      </div>
+                    </Link>
+                  )
+                }
+              })
+            }
+                      
+            
+            </div>
+           </> }
             
           </div>
         </Container>

4974ad4ad8b9acc654bff183a74347ed83c23c9f
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2024-08-08 11:52:24 -0400

    add gtag

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -41,6 +41,23 @@ module.exports = {
       }
     },
     {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "G-8HCTYSWFZ8",
+  
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+      }
+    },
+    {
       resolve: 'gatsby-plugin-local-search',
       options: {
         // A unique name for the search index. This should be descriptive of
